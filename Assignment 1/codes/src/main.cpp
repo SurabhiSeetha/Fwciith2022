3511cec6e5c88556015029bf1dfeabea6a66a48b
SurabhiSeetha <83218082+SurabhiSeetha@users.noreply.github.com>
2022-09-05 10:51:59 +0530

    Update main.cpp

--- Assignment 1/codes/src/main.cpp
+++ Assignment 1/codes/src/main.cpp
@@ -4,31 +4,31 @@ int Z,Y,X,W,F;
 //Code released under GNU GPL.  Free to use for anything.
 void disp_7447(int D, int C, int B, int A)
 {
-  digitalWrite(2, A); //LSB
-  digitalWrite(3, B); 
-  digitalWrite(4, C); 
-  digitalWrite(5, D); //MSB
+  digitalWrite(6, A); //LSB
+  digitalWrite(7, B); 
+  digitalWrite(8, C); 
+  digitalWrite(9, D); //MSB
 
 }
 // the setup function runs once when you press reset or power the board
 void setup() {
-    pinMode(2, OUTPUT);  
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, INPUT);  
-    pinMode(7, INPUT);
-    pinMode(8, INPUT);
-    pinMode(9, INPUT);
+    pinMode(6, OUTPUT);  
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(2, INPUT);  
+    pinMode(3, INPUT);
+    pinMode(4, INPUT);
+    pinMode(5, INPUT);
     
 }
 
 // the loop function runs over and over again forever
 void loop() {
-X = digitalRead(6); //LSB
-Y = digitalRead(7); 
-Z = digitalRead(8); 
-W = digitalRead(9); //MSB
+X = digitalRead(2); //LSB
+Y = digitalRead(3); 
+Z = digitalRead(4); 
+W = digitalRead(5); //MSB
 
 F=(X&&!Y)||(X&&!Z&&!W)||(X&&Z&&W)||(!Y&&!Z)||(!X&&Y&&Z&&!W);
 
