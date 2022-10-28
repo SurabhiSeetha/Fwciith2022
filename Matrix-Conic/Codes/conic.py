42be7865b14da87ae7e0228bc4a935c217cbaab2
SurabhiSeetha <83218082+SurabhiSeetha@users.noreply.github.com>
2022-10-28 15:04:51 +0530

    Update conic.py

--- Matrix-Conic/Codes/conic.py
+++ Matrix-Conic/Codes/conic.py
@@ -1,19 +1,64 @@
 import random as rd
-  2 import numpy as np
-  3 import matplotlib.pyplot as plt
-  4
-  5 #if using termux
-  6 import subprocess
-  7 import shlex
-  8 #end if
-  9
- 10
- 11 x1 = np.linspace(-8,10,100)
- 12 y1 = 1 / (x1 - 3)
- 13 plt.plot(x1,y1, color="Blue")
- 14 plt.grid() # minor
- 15 plt.show()
- 16
- 17
- 18 #plt.savefig('/sdcard/Download/conicfig.pdf')
- 19 #subprocess.run(shlex.split("termux-open '/sdcard/Do    wnload/conicfig.pdf'"))
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+#if using termux
+#import subprocess
+#import shlex
+#end if
+
+#input parameters
+slope = 2
+m = np.array([[1],[slope]])
+n = np.array([[-slope],[1]])
+v = np.array([[0,0.5],[0.5,0]])
+u = np.array([[0],[-3/2]])
+f = -1
+
+vin = np.linalg.inv(v)
+
+f0 = f + u.T @ vin @ u
+
+k = math.sqrt(f0 / (n.T @ vin @ n))
+
+q = vin @ ( k * n - u ) 
+
+
+mu = ( 1 / ( m.T @ v @ m ) ) @ ( - m.T @ ((v @ q) + u ) ) 
+
+xi = q + mu * m
+
+x =xi[0][0]
+y=xi[1][0]
+pt=['P']
+
+plt.scatter(x,y)
+
+for i, txt in enumerate(pt):
+    plt.annotate(txt, # this is the text
+                 (x, y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(2,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+print("The Required Point Of Contact is: ")
+print(xi)
+
+x1 = np.linspace(-6,10,100)
+y1 = 1 / (x1 - 3) 
+plt.plot(x1,y1, color="Blue")
+
+x2 = np.linspace(-6,10,100)
+y2 = [slope * (t - x) + y for t in x2]
+
+plt.plot(x2,y2, color="Red")
+
+plt.xlabel('$X-axis$')
+plt.ylabel('$Y-axis$')
+plt.grid() # minor
+plt.show()
+
+
+plt.savefig('/sdcard/Download/conicfig.pdf')
+subprocess.run(shlex.split("termux-open '/sdcard/Download/conicfig.pdf'"))
